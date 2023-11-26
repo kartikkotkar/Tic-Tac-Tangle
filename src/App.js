a5364dc3ba492fb9b40505b5339ff944d6698ba6
kartikkotkar <kartikkotkar973@gmail.com>
2023-11-25 22:03:46 -0800

    added app.js file + app.css file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
   const [player, setPlayer] = useState(1)
@@ -14,17 +14,80 @@ function App() {
     8: "",
     9: "",
   })
+  const [winner, setWinner] = useState(null);
 
-  const play = (boxNo) =>{
-    if(player===1){
-      setBoard({...board, [boxNo]: "🐼"})
-    }
-    else{
-      setBoard({...board, [boxNo]: "👻"})
+  const checkWinner = () => {
+    const symbol = player === 1 ? "🐼" : "👻";
+
+    if (board[1] === symbol && board[2] === symbol && board[3] === symbol) {
+      setWinner(player);
     }
 
-    setPlayer(player===1 ? 2 : 1)
+    else if (board[4] === symbol && board[5] === symbol && board[6] === symbol) {
+      setWinner(player);
+    }
+
+    else if (board[7] === symbol && board[8] === symbol && board[9] === symbol) {
+      setWinner(player);
+    }
+
+    else if (board[1] === symbol && board[4] === symbol && board[7] === symbol) {
+      setWinner(player);
+    }
+
+    else if (board[2] === symbol && board[5] === symbol && board[8] === symbol) {
+      setWinner(player);
+    }
+
+    else if (board[3] === symbol && board[6] === symbol && board[9] === symbol) {
+      setWinner(player);
+    }
+
+    else if (board[1] === symbol && board[5] === symbol && board[9] === symbol) {
+      setWinner(player);
+    }
+
+    else if (board[3] === symbol && board[5] === symbol && board[7] === symbol) {
+      setWinner(player);
+    }
+    setPlayer(player === 1 ? 2 : 1);
   }
+
+  const play = (boxNo) => {
+    if (board[boxNo] !== "" || winner !== null) {
+      return;
+    }
+
+    if (player === 1) {
+      setBoard({ ...board, [boxNo]: "🐼" })
+    }
+    else {
+      setBoard({ ...board, [boxNo]: "👻" })
+    }
+
+
+  }
+
+  const reset = () => {
+    setPlayer(1);
+    setBoard({
+      1: "",
+      2: "",
+      3: "",
+      4: "",
+      5: "",
+      6: "",
+      7: "",
+      8: "",
+      9: "",
+    })
+    setWinner(null);
+  }
+
+  
+  useEffect(()=>{
+    checkWinner(player);
+  }, [board])
 
 
 
@@ -37,55 +100,67 @@ function App() {
           player 1; 🐼
         </span>
 
+        <span className='text-center current-player-text'>
+        Current Player: {player === 1 ? " 🐼" : "👻"}
+      </span>
+
+
         <span className='player-title'>
           player 2; 👻
         </span>
       </div>
-      <p className='text-center current-player-text'>
-        Current Player: {player === 1 ? " 🐼" : "👻"}
-      </p>
+     
+      {
+        winner ? (
+          <h1 className='winner-text'>  winner is { winner === 1 ? " 🐼" : "👻"}</h1>
+        ) : null
+      }
 
       <div className='board'>
         <div className='row'>
-          <div className='box' onClick={()=>{play(1)}}>
+          <div className='box' onClick={() => { play(1) }}>
             {board[1]}
           </div>
-          <div className='box'  onClick={()=>{play(2)}}>
+          <div className='box' onClick={() => { play(2) }}>
             {board[2]}
           </div>
-          <div className='box'  onClick={()=>{play(3)}}>
+          <div className='box' onClick={() => { play(3) }}>
             {board[3]}
           </div>
         </div>
 
         <div className='row'>
-          <div className='box'  onClick={()=>{play(4)}}>
+          <div className='box' onClick={() => { play(4) }}>
             {board[4]}
           </div>
-          <div className='box'  onClick={()=>{play(5)}}>
+          <div className='box' onClick={() => { play(5) }}>
             {board[5]}
           </div>
-          <div className='box'  onClick={()=>{play(6)}}>
+          <div className='box' onClick={() => { play(6) }}>
             {board[6]}
           </div>
         </div>
 
         <div className='row'>
-          <div className='box'  onClick={()=>{play(7)}}>
+          <div className='box' onClick={() => { play(7) }}>
             {board[7]}
           </div>
-          <div className='box'  onClick={()=>{play(8)}}>
+          <div className='box' onClick={() => { play(8) }}>
             {board[8]}
           </div>
-          <div className='box'  onClick={()=>{play(9)}}>
+          <div className='box' onClick={() => { play(9) }}>
             {board[9]}
           </div>
         </div>
       </div>
 
+      <button type='button' className='reset-btn' onClick={reset}>Reset</button>
+
     </div>
 
-  );
+
+
+  )
 }
 
 export default App;
